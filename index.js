01cf60f44d15eec20f78d21f169b07865efcf0b0
Pablo Quirino <pabloquirino17@gmail.com>
2024-11-05 13:22:32 -0300

    feat: configure Express application with Handlebars and session management
    - Set up Handlebars as the template engine.
    - Implemented middleware for URL-encoded and JSON requests.
    - Added session management with express-session and file storage.
    - Enabled flash messages for user notifications.
    - Configured static file serving from the 'public' directory.

--- index.js
+++ index.js
@@ -8,6 +8,58 @@ import conn from './db/conn.js'
 const FileStore = FileStoreModule(session)
 const app = express()
 
+// template engine
+const hbs = create({
+    extname: 'handlebars'
+})
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+// receive responses from the body
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+// session middleware
+app.use(
+    session({
+        name: "session",
+        secret: 'our_secret',
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function () {},
+            path: require('path').join(require('os').tmpdir(), 'sessions')
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true
+        }
+    })
+)
+
+// flash messages
+app.use(flash())
+
+// public path
+app.use(express.static('public'))
+
+// set session to res
+app.use((req, res, next) => {
+    if (req.session.userid) {
+        res.locals.session = req.session
+    }
+
+    next()
+})
+
 conn 
     .sync()
     .then(() => {
