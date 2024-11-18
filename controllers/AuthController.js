1d9223ccee0aca18d1f3efb58391488bc93ce30c
Pablo Quirino <pabloquirino17@gmail.com>
2024-11-18 19:51:39 -0300

    feat: add password validation to registration route and visual feedback in the form

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,6 @@
+import User from '../model/User.js' 
+import bcrypt from 'bcryptjs'
+
 export default class AuthController {
     static login(req, res) {
         res.render('auth/login')
@@ -5,5 +8,17 @@ export default class AuthController {
 
     static register(req, res) {
         res.render('auth/register')
+    }
+
+    static async registerPost(req, res) {
+        const { name, email, password, confirmpassword } = req.body
+
+        // password match validation
+        if(password != confirmpassword) {
+            req.flash('message', 'As senhas não conferem, tente novamente!')
+            res.render('auth/register')
+
+            return
+        }
     }
 }
